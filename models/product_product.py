22b9204e630adae2d5a5b0c0ab6530a67fe6903c
echovoid14 <echovoid14@gmail.com>
2025-12-11 10:10:37 +0700

    Update multiple owner_id at picking level

--- models/product_product.py
+++ models/product_product.py
@@ -4,4 +4,4 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     owner_id = fields.Many2one(comodel_name='res.partner', related='product_tmpl_id.owner_id', store=True, readonly=False)
-    sales_person_ids = fields.Many2many(comodel_name='res.users', related='product_tmpl_id.sales_person_ids', store=False, help='Sales persons who are responsible for this product template.')
+    sales_person_ids = fields.Many2many(comodel_name='res.users', related='product_tmpl_id.sales_person_ids', help='Sales persons who are responsible for this product template.')
--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,46 +1,34 @@
 # -*- coding: utf-8 -*-
 from odoo import models, api, fields, _
-from odoo.exceptions import UserError
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    owner_id = fields.Many2one(
+    owner_ids = fields.Many2many(
         'res.partner',
-        string="Owner",
-        help="Owner akan otomatis mengikuti owner dari Stock Move pertama."
+        'stock_picking_owner_rel',
+        'picking_id',
+        'owner_id',
+        string="Owners",
+        help="Owner yang terlibat pada stock move."
     )
 
     @api.model
     def create(self, vals):
         picking = super().create(vals)
 
-        if picking.move_ids and not picking.owner_id:
-            picking.owner_id = picking.move_ids[0].owner_id.id
+        if picking.move_ids:
+            owners = picking.move_ids.mapped('owner_id')
+            picking.owner_ids = [(6, 0, owners.ids)]
 
         return picking
 
     def write(self, vals):
         res = super().write(vals)
 
-        # Setelah perubahan, lakukan cek konsistensi owner
         for picking in self:
-
             owners = picking.move_ids.mapped('owner_id')
-            owners = list(set(owners))
 
-            # Jika tidak ada owner, tidak perlu dicek
-            if not owners:
-                continue
-
-            # Jika owner lebih dari 1 → ERROR
-            if len(owners) > 1:
-                raise UserError(
-                    _("Semua stock move pada picking ini harus memiliki owner yang sama.")
-                )
-
-            # Jika picking.owner_id belum diisi, isi otomatis dari owner move
-            if not picking.owner_id:
-                picking.owner_id = owners[0]
+            picking.owner_ids = [(6, 0, owners.ids)]
 
         return res
