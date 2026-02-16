bd32124e667b9e7dff3f0816ec51439d43bf8fbe
echovoid14 <echovoid14@gmail.com>
2026-02-16 16:12:45 +0700

    Fix No. BTB bisa tergenerate walau penerimaan belum divalidasi dan label owner nya tolong dihide saja Pak  agar tidak bingung

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -2,17 +2,33 @@
 from odoo import models, api, fields, _
 from dateutil.relativedelta import relativedelta
 from datetime import timedelta
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    pemilik_ids = fields.Many2many('res.partner', 'stock_picking_owner_rel', 'picking_id', 'owner_id', string="Owners", help="Owner yang terlibat pada stock move.")
+    _sql_constraints = [
+        ('btb_number_unique', 'unique(btb_number)', 'BTB number must be unique!')
+    ]
+
+    pemilik_ids = fields.Many2many(
+        'res.partner',
+        'stock_picking_owner_rel',
+        'picking_id',
+        'owner_id',
+        string="Owners",
+        help="Owner yang terlibat pada stock move."
+    )
+
     btb_number = fields.Char(string="No. BTB", readonly=True, copy=False)
-    partner_ref = fields.Char('Vendor Reference', copy=False,
-        help="Reference of the sales order or bid sent by the vendor. "
-             "It's used to do the matching when you receive the "
-             "products as this reference is usually written on the "
-             "delivery order sent by your vendor.")    
+
+    partner_ref = fields.Char(
+        'Vendor Reference',
+        copy=False,
+        help="Reference of the sales order or bid sent by the vendor."
+    )
 
     def _get_bulan_romawi(self, bulan):
         romawi = {
@@ -22,69 +38,75 @@ class StockPicking(models.Model):
         }
         return romawi.get(bulan, '')
 
-    def button_validate(self):
-        res = super().button_validate()
-
-        for picking in self.filtered(lambda p: p.picking_type_code == 'incoming'):
-            if not picking.btb_number:
-                date_done = picking.date_done or fields.Datetime.now()
-                date_done_dt = fields.Datetime.context_timestamp(picking, date_done)
-
-                tahun = date_done_dt.strftime('%y')
-                bulan_romawi = self._get_bulan_romawi(date_done_dt.month)
-
-                domain = [
-                    ('picking_type_code', '=', 'incoming'),
-                    ('btb_number', '!=', False),
-                    ('date_done', '>=', date_done_dt.replace(day=1, hour=0, minute=0, second=0, microsecond=0)),
-                    ('date_done', '<=', date_done_dt.replace(day=1, hour=23, minute=59, second=59, microsecond=999999)
-                                + relativedelta(months=1) - timedelta(microseconds=1))
-                ]
-                last = self.env['stock.picking'].search(domain, order='btb_number desc', limit=1)
-
-                if last:
-                    last_urutan = int(last.btb_number.split('/')[1])
-                    urutan = last_urutan + 1
-                else:
-                    urutan = 1
-
-                warehouse = picking.picking_type_id.warehouse_id
-                warehouse_code = warehouse.code if warehouse else 'NA'
-
-                btb = 'BTB/%02d/%s/%s/%s' % (
-                    urutan,
-                    bulan_romawi,
-                    tahun,
-                    warehouse_code
-                )
-                picking.btb_number = btb
-
-                # update PO
-                po_ids = picking.move_ids.mapped('purchase_line_id.order_id')
-                if po_ids:
-                    po_ids.write({'btb_number': btb})
-
-        return res
-
     @api.model
     def create(self, vals):
         picking = super().create(vals)
 
-        if picking.move_ids:
-            owners = picking.move_ids.mapped('owner_id')
-            picking.with_context(skip_owner_sync=True).pemilik_ids = [(6, 0, owners.ids)]
+        # hanya untuk incoming
+        if picking.picking_type_code != 'incoming' or picking.btb_number:
+            return picking
 
+        date_now = fields.Datetime.context_timestamp(
+            picking, fields.Datetime.now()
+        )
+
+        tahun = date_now.strftime('%y')
+        bulan_romawi = self._get_bulan_romawi(date_now.month)
+
+        start_month = date_now.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        end_month = (start_month + relativedelta(months=1)) - timedelta(microseconds=1)
+
+        domain = [
+            ('picking_type_code', '=', 'incoming'),
+            ('btb_number', '!=', False),
+            ('create_date', '>=', start_month),
+            ('create_date', '<=', end_month),
+        ]
+
+        last = self.env['stock.picking'].sudo().search(
+            domain,
+            order='id desc',
+            limit=1
+        )
+
+        if last and last.btb_number:
+            try:
+                last_urutan = int(last.btb_number.split('/')[1])
+                urutan = last_urutan + 1
+            except Exception:
+                urutan = 1
+        else:
+            urutan = 1
+
+        warehouse = picking.picking_type_id.warehouse_id
+        warehouse_code = warehouse.code if warehouse else 'NA'
+
+        btb_number = 'BTB/%02d/%s/%s/%s' % (
+            urutan,
+            bulan_romawi,
+            tahun,
+            warehouse_code
+        )
+
+        picking.sudo().write({'btb_number': btb_number})
+
+        if picking.origin:
+            po = self.env['purchase.order'].sudo().search([
+                ('name', '=', picking.origin)
+            ], limit=1)
+
+            if po:
+                po.write({'btb_number': btb_number})
         return picking
 
     def write(self, vals):
         res = super().write(vals)
 
-        if self.env.context.get('skip_owner_sync'):
-            return res
-
-        for picking in self:
-            owners = picking.move_ids.mapped('owner_id')
-
-            picking.with_context(skip_owner_sync=True).pemilik_ids = [(6, 0, owners.ids)]
+        if 'move_ids' in vals:
+            for picking in self:
+                owners = picking.move_ids.mapped('owner_id')
+                picking.with_context(skip_owner_sync=True).sudo().update({
+                    'pemilik_ids': [(6, 0, owners.ids)]
+                })
 
         return res
